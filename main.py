d77c9d066c7db4dcf0a194195f70af8af0e76981
Satyendra <satyendra.growth19@gmail.com>
2026-05-23 16:23:21 +0530

    rename project

---
+++ main.py
@@ -0,0 +1,45 @@
+tasks = []
+
+while True:
+    print("\n=== CLI ToDo App ===")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter new task: ")
+        tasks.append(task)
+        print("Task added successfully!")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("\nYour Tasks:")
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+    elif choice == "3":
+        if len(tasks) == 0:
+            print("No tasks to delete.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}")
+
+            task_num = int(input("Enter task number to delete: "))
+
+            if 1 <= task_num <= len(tasks):
+                removed = tasks.pop(task_num - 1)
+                print(f"Task '{removed}' deleted.")
+            else:
+                print("Invalid task number.")
+
+    elif choice == "4":
+        print("Exiting ToDo App...")
+        break
+
+    else:
+        print("Invalid choice. Try again.")
